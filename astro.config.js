e208ef4818cdec6218ff6308d60683c9b98cc4ba
Matt B <matt@optimizewebsolutions.com>
2024-10-13 00:10:50 -0400

    Style tweaks

--- astro.config.js
+++ astro.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
+  output: "static",
+  adapter: vercel(),
+  server: {
+    port: 3000,
+  },
+  site: "https://mattbirch.co",
   integrations: [
     tailwind({
       config: { path: "./tailwind.config.js" },
@@ -12,10 +19,4 @@ export default defineConfig({
     react(),
     icon(),
   ],
-  site: "https://mattbirch.co",
-  output: "static",
-  adapter: vercel(),
-  server: {
-    port: 3000,
-  },
 });
