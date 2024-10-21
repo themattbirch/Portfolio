b43192d6c017e24c0db84e9fd98506f248f02688
Matt B <matt@optimizewebsolutions.com>
2024-10-21 01:53:58 -0400

    Edits

--- astro.config.js
+++ astro.config.js
@@ -2,21 +2,27 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
-import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  output: "static",
-  adapter: vercel(),
-  server: {
-    port: 3000,
-  },
-  site: "https://mattbirch.co",
+  site: process.env.BASE_URL || "https://mattbirch.co",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+    host: process.env.HOST || "0.0.0.0",
+    port: process.env.PORT || 3001,
+  }),
   integrations: [
     tailwind({
       config: { path: "./tailwind.config.js" },
     }),
     react(),
     icon(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
   ],
 });
