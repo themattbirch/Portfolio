27406ec0dd993c526c65c8ba735849f28f4894ee
Matt B <matt@optimizewebsolutions.com>
2024-11-07 20:19:02 -0500

    Edits

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,6 +6,12 @@ declare global {
   interface Window {
     initializeSkillToggles?: () => void;
     updateTheme: (theme: string) => void;
+    dataLayer?: any[];
+    gtag?: {
+      (command: 'js', date: Date): void;
+      (command: 'config', targetId: string, config?: object): void;
+      (command: string, ...args: any[]): void;
+    };
   }
 }
 
