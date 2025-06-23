fbeb59ee3a3525063b09bfedc6e2b6f59a61953b
Matt B <matt@optimizewebsolutions.com>
2025-06-23 17:56:47 -0400

    Adjusted start script to add hostname

--- astro.config.js
+++ astro.config.js
@@ -1,15 +1,15 @@
-// astro.config.js
+// astro.config.mjs
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
 
 export default defineConfig({
   site: process.env.BASE_URL || "https://mattbirch.co",
   output: "server",
-  adapter: node({ mode: "standalone" }), // no host/port here
+  adapter: node({ mode: "standalone" }), // ← no host/port here
   integrations: [
     tailwind({ config: { path: "./tailwind.config.js" } }),
     react(),
